da5b36feadf3cb28f898b81fee01be10c8c31c29
Caleb Studdie <calebstuddie@gmail.com>
2025-09-01 08:04:29 -0500

    Front-End Initialization

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind is Working 🎉
+      </h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
